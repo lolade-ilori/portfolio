a80fcf0180f9d11d21db0b1b83f2bc9e5f1bfe01
Lolade Ilori <loladeilori02@gmail.com>
2025-05-25 23:44:37 +0100

    worked on the sidebar

--- app/(personal)/Sidebar.tsx
+++ app/(personal)/Sidebar.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import bg from "../assets/img/Background.png";
 import linkedIn from "../assets/img/ion_logo-linkedin.png";
 import github from "../assets/img/ri_github-fill.png";
+import sidebar1 from "../assets/img/sidebar1.png";
+import sidebar2 from "../assets/img/sidebar2.png";
 
 const Sidebar = () => {
   return (
@@ -21,6 +23,14 @@ const Sidebar = () => {
             lolade ilori.
           </Link>
           <h3 className="text-[#545D69]">available</h3>
+        </div>
+
+        <div className="flex relative h-screen flex-col justify-end pb-35">
+          <div className="relative max-h-100 flex overflow-hidden w-full items-center gap-2">
+            <Image src={sidebar1} alt="sidebarimg" />
+
+            <Image src={sidebar2} alt="sidebarimg" />
+          </div>
         </div>
 
         <div className="fixed left-0 bottom-0 z-20 font-faro-local pb-20 px-9">
